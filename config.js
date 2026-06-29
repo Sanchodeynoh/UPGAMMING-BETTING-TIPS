c76dc6b80feae98c5b1b68c5a1a08670a7067e24
Denis Kamau <denomsancho5@gmail.com>
2026-06-29 22:22:32 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-// IMPORTANT: After you deploy your backend on Render, replace the URL below
-// with your actual Render service URL, e.g.:
-// const API_BASE_URL = "https://upgamming-backend.onrender.com";
-
-const API_BASE_URL = "https://upgamming-betting-tips-backend.onrender.com";
